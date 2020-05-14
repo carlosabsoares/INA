26d352fa943645e38bc9e63e73e2460213f60a0b
carlos alberto baptista soares <carlosabsoares@gmail.com>
2020-05-14 15:23:06 -0300

    etapa 1

---
+++ RPGCombatKata.Domain/Entities/Characters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPGCombatKata.Domain.Entities
+{
+    public class Characters
+    {
+        public string Name { get; set; }
+
+        public int Health { get; private set; }
+        public int Level { get; private set; }
+        public bool Alive { get; private set; }
+
+        public Characters()
+        {
+            Health = 1000;
+            Alive = true;
+            Level = 1;
+        }
+
+    }
+}
