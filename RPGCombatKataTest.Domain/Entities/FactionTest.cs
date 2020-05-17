285897e3f3b71742d3d89bb2b45912b0d7ecdb66
carlos alberto baptista soares <carlosabsoares@gmail.com>
2020-05-17 11:17:52 -0300

    Revisao

---
+++ RPGCombatKataTest.Domain/Entities/FactionTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RPGCombatKata.Domain.Entities;
+
+namespace RPGCombatKataTest.Domain.Entities
+{
+    [TestClass]
+    public class FactionTest
+    {
+        [TestMethod]
+        [TestCategory("Character")]
+        public void ValidaCriacaoDeFaccao()
+        {
+            string _nomeFaction = "Titans";
+            var _gangs = new IFaction(_nomeFaction);
+
+            Assert.AreEqual(_gangs.Name, _nomeFaction);
+        }
+
+        private class IFaction : Faction
+        {
+            public override string Name { get; }
+
+            public IFaction(string nome)
+            {
+                Name = nome;
+            }
+        }
+    }
+}
